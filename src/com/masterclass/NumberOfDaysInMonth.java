86d6547521ef03b2f24c2db322a34232d6bef92f
edielam <edguard738@gmail.com>
2022-01-20 23:03:47 +0000

    no of days in a month

---
+++ src/com/masterclass/NumberOfDaysInMonth.java
@@ -0,0 +1,55 @@
+package com.masterclass;
+
+public class NumberOfDaysInMonth {
+        // write your code here
+        public static boolean isLeapYear(int year){
+            if(year<1 || year >9999){
+                return false;
+            }
+            else if((year % 4 == 0 && year % 100 != 0) || year % 400 ==0){
+                return true;
+            }
+            return false;
+        }
+        public static int getDaysinMonth(int month, int year){
+            if (month <1 || month > 12){
+                return -1;
+            }
+            if (year <1 || year > 9999){
+                return -1;
+            }
+            int days;
+            switch(month){
+                case 2:
+                    if(isLeapYear(year)){
+                        days = 29;
+                    } else
+                        days = 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                    days = 30;
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 11:
+                case 12:
+                    days = 31;
+                    break;
+                default:
+                    days = -1;
+                    break;
+            }
+            return days;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(getDaysinMonth(2,2016));
+    }
+}
+
