d5ab046f5078220d61cc275af696f5bc02a97135
edielam <edguard738@gmail.com>
2022-01-20 12:49:24 +0000

    switch

--- src/com/masterclass/PlayingCat.java
+++ src/com/masterclass/PlayingCat.java
@@ -14,5 +14,9 @@ public class PlayingCat {
     }
     public static void main(String[] args){
 		System.out.println(isCatPlaying(false, 35));
+		int[] arr = {5,4,3,2,1};
+		for(int i = 0; i < arr.length; i++){
+			System.out.print(arr[i] - (arr.length-i));
+		}
     }
 }
---
+++ src/com/masterclass/SwitchBiz.java
@@ -0,0 +1,24 @@
+package com.masterclass;
+
+public class SwitchBiz {
+    public static void main(String[] args){
+	char ch = 'F';
+	switch(ch) {
+	case 'A':
+	    System.out.println("Char is A");
+	    break;
+	case 'B':
+	    System.out.println("Char is B");
+	    break;
+	case 'C':
+	    System.out.println("Char is C");
+	    break;
+	case 'D':
+	    System.out.println("Char is D");
+	    break;
+	default:
+	    System.out.println("Not found");
+	    break;
+	}
+    }
+}
