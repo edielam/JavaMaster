3f5d79168d473746c4e49c529de0a6ef80d7069f
edielam <edguard738@gmail.com>
2022-01-23 23:20:19 +0000

    task 18

--- src/com/masterclass/SharedDigit.java
+++ src/com/masterclass/SharedDigit.java
@@ -1,30 +1,33 @@
 package com.masterclass;
 
 public class SharedDigit{
-    public static boolean hasSharedDigit(int num1, int num2){
-		int digit1;
-		int digit2;
-		boolean answer = true;
-		if((num1 < 10 || num1 > 99) || (num2 < 10 || num2 > 99)){
+    public static boolean hasSharedDigit(int num1, int num2) {
+		int digit1 = num1 % 10;
+		int digit2 = num1 / 10;
+		boolean answer = false;
+		if ((num1 < 10 || num1 > 99) || (num2 < 10 || num2 > 99)) {
 			return false;
 		}
-		while(num1 > 0){
-			digit1 = num1 % 10;
-			while(num2 > 0){
-				digit2 = num2 % 10;
-				if(digit2 == digit1){
-					answer = true;
-				}
-				else{
-					answer = false;
-				}
-				num2 /= 10;
-			}
-			num1 /= 10;
+
+		if(digit1 == num2%10){
+			answer = true;
 		}
+		else if(digit1 == num2/10){
+			answer = true;
+		}
+		else if(digit2 == num2%10){
+			answer = true;
+		}
+		else if(digit2 == num2/10){
+			answer = true;
+		}
+		else {
+			answer = false;
+		}
+
 		return answer;
-    }
+	}
     public static void main(String[] args){
-		System.out.println(hasSharedDigit(12, 32));
+		System.out.println(hasSharedDigit(115, 67));
     }
 }
