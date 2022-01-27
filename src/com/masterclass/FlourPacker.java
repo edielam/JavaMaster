d4a42f30169cadbe0259b3878772399743da3a0c
edielam <edguard738@gmail.com>
2022-01-27 00:25:04 +0000

    23

--- src/com/masterclass/FlourPacker.java
+++ src/com/masterclass/FlourPacker.java
@@ -1,8 +1,19 @@
 package com.masterclass;
 
 public class FlourPacker {
-    // write your code here
     public static boolean canPack(int bigCount, int smallCount, int goal){
+        if(bigCount< 0 || smallCount < 0 || goal < 0){
+            return false;
+        }
+        if((5*bigCount)+smallCount >= goal){
+            if((5*bigCount) < goal){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(canPack(1, 0, 14));
     }
 }
