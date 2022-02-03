d2f5509938165b0ea11a47c3edcd3e4b63b76b2c
edielam <edguard738@gmail.com>
2022-02-03 23:27:41 +0000

    composition

---
+++ section 6/src/com/masterclass/Lamp.java
@@ -0,0 +1,24 @@
+package com.masterclass;
+
+public class Lamp{
+    private String style;
+    private boolean battery;
+    private int globRating;
+    public Lamp(Sting style, boolean battery, int globRating){
+	this.style = style;
+	this.battery = battery;
+	this.globRating = globRating;
+    }
+    public void turnOn(){
+	System.out.println("Lamp is turned on");
+    }
+    public String getStyle(){
+	return style;
+    }
+    public boolean isBattery(){
+	return battery;
+    }
+    public int getGlobRating(){
+	return globRating;
+    }
+}
