65c2f3569b9afa52b20cad8aa4d68164df36633a
edielam <edguard738@gmail.com>
2022-02-04 11:04:07 +0000

    encapsulation

--- section 6/src/com/masterclass/Printer.java
+++ section 6/src/com/masterclass/Printer.java
@@ -6,33 +6,34 @@ public class Printer{
     private boolean duplex;
 
     public Printer(int tonerLevel, boolean duplex){
-	if(tonerLevel > -1 && tonerLevel<= 100){
-	    this.tonerLevel = tonerLevel; 
-	}
-	else{
-	    this.tonerLevel = -1;
-	}
-	this.duplex = duplex;
-	this.pagesPrinted = 0;
+		if(tonerLevel > -1 && tonerLevel<= 100){
+			this.tonerLevel = tonerLevel;
+		}
+		else{
+			this.tonerLevel = -1;
+		}
+		this.duplex = duplex;
+		this.pagesPrinted = 0;
     }
     public int addToner(int tonerAmount){
-	if(tonerAmount > 0 && tonerAmount <=100){
-	    if(tonerAmount + this.tonerLevel <= 100){
-		return this.tonerLevel += tonerAmount
-		    }else {
+		if(tonerAmount > 0 && tonerAmount <=100){
+			if(tonerAmount + this.tonerLevel <= 100){
+			return this.tonerLevel += tonerAmount;
+				}else {
+			return -1;
+			}
+		}
 		return -1;
-	    }
-	}
-	return -1;
     }
     public int printPages(int pages){
-	int pagesToPrint = pages;
-	if(duplex == true){
-	    this.pagesPrinted += pagesToPrint;
-	}
-	return pagesToPrint;
+		int pagesToPrint = pages;
+		if(duplex == true){
+			this.pagesPrinted += pagesToPrint;
+			pagesToPrint = (int)Math.ceil(pages / 2.0);
+		}
+		return pagesToPrint;
     }
     public int getPagesPrinted(){
-	return this.pagesPrinted;
+		return this.pagesPrinted;
     }
 }
--- section 6/src/com/masterclass/Test.java
+++ section 6/src/com/masterclass/Test.java
@@ -22,10 +22,16 @@ public class Test {
 //        Floor floor = new Floor(5.4,4.5);
 //        Calculator calculator = new Calculator(floor, carpet);
 //        System.out.println(calculator.getTotalCost());
-        Circle circle = new Circle(3.75);
-        System.out.println(circle.getRadius());
-        Cylinder cylinder = new Cylinder(5.55, 7.25);
-        System.out.println(cylinder.getVolume());
-
+//        Circle circle = new Circle(3.75);
+//        System.out.println(circle.getRadius());
+//        Cylinder cylinder = new Cylinder(5.55, 7.25);
+//        System.out.println(cylinder.getVolume());
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(50));
+        int p = printer.printPages(4);
+        System.out.println(printer.getPagesPrinted());
+        int q = printer.printPages(8);
+        System.out.println(printer.getPagesPrinted());
+        System.out.println(q);
     }
 }
