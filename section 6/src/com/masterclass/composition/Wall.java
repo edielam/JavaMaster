cbeef1d6ea8701a424c238206707cb121069a2e3
edielam <edguard738@gmail.com>
2022-02-04 00:32:42 +0000

    composition

---
+++ section 6/src/com/masterclass/composition/Wall.java
@@ -0,0 +1,12 @@
+package com.masterclass.composition;
+
+public class Wall{
+    private String direction;
+
+    public Wall(String direction){
+	    this.direction = direction;
+    }
+    public String getDirection(){
+	    return this.direction;
+    }
+}
