25af9ef0250c3cd3833e6ba578e73d7c41653c25
edielam <edguard738@gmail.com>
2022-02-07 01:07:41 +0000

    poly

--- section 6/src/com/masterclass/poly/Mitsubishi.java
+++ section 6/src/com/masterclass/poly/Mitsubishi.java
@@ -2,26 +2,26 @@ package com.masterclass.poly;
 
 public class Mitsubishi extends Car{
     public Mitsubishi(int cylinders, String name){
-	super(cylinders, name);
+	    super(cylinders, name);
     }
     @Override
     public String startEngine(){
-	return super.startEngine();
+	    return super.startEngine();
     }
     @Override
     public String accelerate(){
-	return super.accelerate();
+	    return super.accelerate();
     }
     @Override
     public String brake(){
-	return super.brake()
+	    return super.brake();
     }
     @Override
     public int getCylinders(){
-	return super.getCylinders();
+	    return super.getCylinders();
     }
     @Override
-    public Sting getName(){
-	return super.getName();
+    public String getName(){
+	    return super.getName();
     }
 }
