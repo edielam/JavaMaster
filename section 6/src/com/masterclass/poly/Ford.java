78dc8a10d30edd09329576a1ec28266876a5f333
edielam <edguard738@gmail.com>
2022-02-07 01:02:50 +0000

    poly

---
+++ section 6/src/com/masterclass/poly/Ford.java
@@ -0,0 +1,27 @@
+package com.masterclass.poly;
+
+public class Ford extends Car{
+    public Ford(int cylinders, String name){
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine(){
+        return super.startEngine();
+    }
+    @Override
+    public String accelerate(){
+        return super.accelerate();
+    }
+    @Override
+    public String brake(){
+        return super.brake()
+    }
+    @Override
+    public int getCylinders(){
+        return super.getCylinders();
+    }
+    @Override
+    public Sting getName(){
+        return super.getName();
+    }
+}
