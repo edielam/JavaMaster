12661c2b8f4f3c3eb5ce85c37070b0b94a162ba5
edielam <edguard738@gmail.com>
2022-02-07 00:59:02 +0000

    poly

--- section 6/src/com/masterclass/poly/Mitsubishi.java
+++ section 6/src/com/masterclass/poly/Mitsubishi.java
@@ -1,5 +1,27 @@
 package com.masterclass.poly;
 
-public class Mitsubuishi extends Car{
-
+public class Mitsubishi extends Car{
+    public Mitsubishi(int cylinders, String name){
+	super(cylinders, name);
+    }
+    @Override
+    public String startEngine(){
+	return super.startEngine();
+    }
+    @Override
+    public String accelerate(){
+	return super.accelerate();
+    }
+    @Override
+    public String brake(){
+	return super.brake()
+    }
+    @Override
+    public int getCylinders(){
+	return super.getCylinders();
+    }
+    @Override
+    public Sting getName(){
+	return super.getName();
+    }
 }
