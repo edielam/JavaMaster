e66a3e3a00b97a695784788b974ffd1478f639e0
edielam <edguard738@gmail.com>
2022-02-08 23:40:35 +0000

    leetcode

--- section 5/src/com/masterclass/Palindrome.java
+++ section 5/src/com/masterclass/Palindrome.java
@@ -3,21 +3,26 @@ package com.masterclass;
 public class Palindrome{
     private int x;
     public Palindrome(int x){
-	this.x = x;
+		this.x = x;
+	}
+	public int isPalindrome(){
+		int y = 0;
+		int test = this.x;
+		while(test > 9){
+			y = y*10 + (test % 10);
+			test /= test;
+		}
+		if( test <= 9){
+			y = (y*10) +test;
+		}
+		if (y == this.x){
+			return y;
+		}
+		return y;
     }
-    public boolean isPalindrome(){
-	int y = 0;
-	int test = this.x;
-	while(test > 9){
-	    y = y*10 + (test % 10);
-	    test /= test;
+
+	public static void main(String[] args) {
+		Palindrome palindrome = new Palindrome(121);
+		System.out.println(palindrome.isPalindrome());
 	}
-	if( test <= 9){
-	    y = (y*10) +test;
-	}
-	if (y == this.x){
-	    return true;
-	}
-	return false;
-    }
 }
