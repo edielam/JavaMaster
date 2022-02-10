7ff9e14cd8cd0db659aa73e34f78c2024be0af76
edielam <edguard738@gmail.com>
2022-02-10 22:30:58 +0000

    arrayList

---
+++ Arrays/src/com/arrays/MobilePhone.java
@@ -0,0 +1,18 @@
+package com.arrays;
+
+import java.util.ArrayList;
+
+public class MobilePhone{
+    private String myNumber;
+        private ArrayList<Contact> myContacts;
+        public MobilePhone(String num){
+        this.myNumber = num;
+        this.myContacts = new ArrayList<Contact>();
+
+        public boolean addNewContact(Contact name){
+                for(int i = 0; i < this.myContacts.length(); i++){
+                    if()
+                }
+        }
+    }
+}
