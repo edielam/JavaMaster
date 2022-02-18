fe718134eec317ab410c151255e81fbedb74c5ee
edielam <edguard738@gmail.com>
2022-02-18 14:17:31 +0000

    arrayList

--- Arrays/src/com/arrays/Contact.java
+++ Arrays/src/com/arrays/Contact.java
@@ -1,5 +1,21 @@
 package com.arrays;
 
+import java.io.StringBufferInputStream;
+
 public class Contact {
-    
+    private String name;
+    private String phoneNumber;
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getName(){
+        return this.name;
+    }
+    private String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public static Contact createContact(String name, String phoneNumber){
+        return new Contact(name, phoneNumber);
+    }
 }
--- Arrays/src/com/arrays/MobilePhone.java
+++ Arrays/src/com/arrays/MobilePhone.java
@@ -4,15 +4,39 @@ import java.util.ArrayList;
 
 public class MobilePhone{
     private String myNumber;
-        private ArrayList<Contact> myContacts;
-        public MobilePhone(String num){
+    private ArrayList<Contact> myContacts;
+    public MobilePhone(String num) {
         this.myNumber = num;
         this.myContacts = new ArrayList<Contact>();
-
-        public boolean addNewContact(Contact name){
-                for(int i = 0; i < this.myContacts.length(); i++){
-                    if()
-                }
+    }
+    public boolean addNewContact(Contact name){
+        if(findContact(name.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
         }
+        myContacts.add(name);
+        return true;
+    }
+    private int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
+    private int findContact(String name){
+        for(int i =0; i<this.myContacts.size(); i++){
+            Contact contact = this.myContacts.get(i);
+            if(contact.getName().equals(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition < 0){
+            System.out.println(oldContact.getName()+", was not found.");
+            return false;
+        }
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName()+", was replaced with "+newContact.getName());
+        return true;
     }
 }
